b220246449c70a45403b5b2334bc8272e740c2ad
Johnlensemble <105088025+Johnlensemble@users.noreply.github.com>
2026-03-06 12:56:43 -0800

    Update pom.xml, InventoryApplication.java, and 5 more files...

--- src/main/java/com/inventory/InventoryApplication.java
+++ src/main/java/com/inventory/InventoryApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Run with: mvn spring-boot:run
  * Or build and run the jar: mvn package && java -jar target/inventory-api-1.0.0.jar
  */
-// Java 21: Enable virtual threads by adding spring.threads.virtual.enabled=true to application.properties
 @SpringBootApplication
 public class InventoryApplication {
     public static void main(String[] args) {
